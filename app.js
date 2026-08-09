da2b799e9e1bd5784622664639645bb97d6498c8
college <codeswithdeepak>
2026-08-09 15:03:14 +0530

    Fix MongoDB connection

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const userRouter = require("./routes/user.js");
 
 // ================= DATABASE =================
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGODB_URI;
 
 main()
     .then(() => {
